41ce64909b897d343e0d64c97bdc091a73d849b0
lucasacw05 <lucasacw@gmail.com>
2016-05-24 14:19:36 -0400

    First Try

---
+++ groceryList/groceryList/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace groceryList
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+		
+
+			//Console.WriteLine (done[0]);
+
+			List<String> [] needHave= new List<String> [5];
+
+			//This is the Need List
+			needHave[0]=new List<string>();
+			needHave[0].Add("Eggs");
+			needHave[0].Add("Bacon");
+			needHave[0].Add("Bread");
+			needHave[0].Add("Chips");
+
+
+
+			//This is the Have list
+			needHave[1]=new List<string>();
+			needHave[1].Add("Salad");
+			needHave[1].Add("Cheese");
+			needHave[1].Add("Water");
+			needHave[1].Add("Fruits");
+
+			Console.WriteLine (needHave[0][0]);
+
+		}
+	}
+}
